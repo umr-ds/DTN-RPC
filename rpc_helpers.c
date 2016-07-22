6fae91afd2a02a5c998d60421c89f6db6d956348
Artur <github@artursterz.de>
2016-07-22 13:25:41 +0200

    Renamed helpers.c to rpc_helpers.c

--- rpc_helpers.c
+++ rpc_helpers.c
@@ -29,7 +29,7 @@ uint8_t *_rpc_prepare_call_payload (uint8_t *payload, const int paramc, const ch
         memcpy(&payload[4], rpc_name, strlen(rpc_name));
         // ... and the parameters.
         memcpy(&payload[4 + strlen(rpc_name)], flat_params, strlen(flat_params));
-        
+
         return payload;
 }
 
