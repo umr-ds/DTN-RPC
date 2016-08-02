f4c9291b56dc21f6657196a287f5da1ad70c3e6b
Artur <github@artursterz.de>
2016-08-02 13:30:37 +0200

    Added CLI options for different call options.

--- main.c
+++ main.c
@@ -21,6 +21,34 @@ void _rpc_server_sig_handler (int signum) {
     running = 1;
 }
 
+void _rpc_print_usage (int mode, char *reason) {
+	switch (mode) {
+		case 1:
+			pfatal("%s\nUsage (server): ./servalrpc (--listen | -l) [(--msp | -1) | (--mdp | -2) | (--rhizome | -3)]", reason);
+			break;
+		case 2:
+			pfatal("%s\nUsage (client): ./servalrpc (--call | -c) [(--msp | -1) | (--mdp | -2) | (--rhizome | -3)] -- (<server_sid> | broadcast | any) <procedure> <arg_1> [<arg_2> ...]", reason);
+			break;
+		default:
+			pfatal( "%s\n"
+					"Usage (server): ./servalrpc (--listen | -l) [(--msp | -1) | (--mdp | -2) | (--rhizome | -3)]\n"
+					"Usage (client): ./servalrpc (--call | -c) [(--msp | -1) | (--mdp | -2) | (--rhizome | -3)] -- (<server_sid> | broadcast | any) <procedure> <arg_1> [<arg_2> ...]",
+					reason
+			);
+	}
+}
+
+int _rpc_check_cli (char *arg, char *option, char *abbrev) {
+	char lng_option[2 + strlen(option)];
+	sprintf(lng_option, "--%s", option);
+	char lng_abbrev[1 + strlen(abbrev)];
+	sprintf(lng_abbrev, "-%s", abbrev);
+	int option_res = strncmp(arg, lng_option, strlen(lng_option)) == 0;
+	int abbrev_res = strncmp(arg, lng_abbrev, strlen(lng_abbrev)) == 0;
+
+	return option_res || abbrev_res;
+}
+
 int main (int argc, char **argv) {
 	// First, check if servald is running.
 	if (server_pid() == 0) {
@@ -30,37 +58,46 @@ int main (int argc, char **argv) {
 
 	// Make sure all required params are set.
 	if (argc < 2) {
-		pfatal("Not enough arguments. Aborting.\n"
-				"Usage for starting the RPC server: %s listen\n"
-				"Usage for starting the RPC client: %s call (<server_sid> | broadcast | any) <procedure> <arg_1> [<arg_2> ...]",
-				argv[0], argv[0]);
+		_rpc_print_usage(0, "Not enough arguments!");
 		return -1;
 	}
 
     _open_keyring();
 
 	// This is the server part. We just listen.
-	if (strncmp(argv[1], "listen", strlen("listen")) == 0) {
+	if (_rpc_check_cli(argv[1], "listen", "l")) {
     	signal(SIGINT, _rpc_server_sig_handler);
     	signal(SIGTERM, _rpc_server_sig_handler);
 		if (argc == 3) {
-			if (strncasecmp(argv[2], "msp", strlen("msp")) == 0) {
+			if (_rpc_check_cli(argv[2], "msp", "1")) {
 				return rpc_server_listen_msp();
-			} else if (strncasecmp(argv[2], "mdp", strlen("mdp")) == 0) {
+			} else if (_rpc_check_cli(argv[2], "mdp", "2")) {
 				return rpc_server_listen_mdp_broadcast();
-			} else if (strncasecmp(argv[2], "rhizome", strlen("rhizome")) == 0) {
+			} else if (_rpc_check_cli(argv[2], "rhizome", "3")) {
 				return rpc_server_listen_rhizome();
+			} else {
+				_rpc_print_usage(1, "Unrecognized option.");
+				return -1;
 			}
+		} else if (argc == 2) {
+			return rpc_server_listen();
+		} else {
+			_rpc_print_usage(1, "Too many options.");
 		}
-		return rpc_server_listen();
-	}
-	else {
-		// Parse params.
-		const char *sidhex = argv[2];
-		const char *name = argv[3];
-		const char *param1 = argv[4];
+	} else if (_rpc_check_cli(argv[1], "call", "c")) {
+		int index = 0;
+		if (_rpc_check_cli(argv[2], "-", "-")) {
+			index = 3;
+		} else {
+			index = 4;
+		}
 
-		if (strncmp(sidhex, "any", 3) == 0) {
+		// Parse params.
+		const char *sidhex = argv[index];
+		const char *name = argv[index + 1];
+		const char *param1 = argv[index + 2];
+
+		if (strncmp(sidhex, "any", strlen("any")) == 0) {
 			sidhex = "broadcast";
 		}
 		sid_t sid;
@@ -70,7 +107,7 @@ int main (int argc, char **argv) {
 		}
 
 		// Get length of additional arguments...
-		unsigned int nfields = (argc == 5) ? 0 : argc - 5;
+		unsigned int nfields = (argc == index + 4) ? 0 : argc - (index + 4);
 		// and create new parameter array of the particular length.
 		const char *params[nfields + 1];
 		params[0] = param1;
@@ -82,13 +119,30 @@ int main (int argc, char **argv) {
 			params[i + 1] = argv[n];
 		}
 
-		int ret_code = rpc_call(sid, name, nfields + 1, params);
+		int ret_code = -1;
+		if (_rpc_check_cli(argv[2], "msp", "1")) {
+			pinfo("Calling directly via MSP");
+			ret_code = rpc_client_call_msp(sid, name, nfields + 1, params);
+		} else if (_rpc_check_cli(argv[2], "mdp", "2")) {
+			pinfo("Calling via MDP broadcasts");
+			ret_code = rpc_client_call_mdp_broadcast(name, nfields + 1, params);
+		} else if (_rpc_check_cli(argv[2], "rhizome", "3")) {
+			pinfo("Calling delay-tolerant via Rhizome");
+			ret_code = rpc_client_call_rhizome(sid, name, nfields + 1, params);
+		} else if (_rpc_check_cli(argv[2], "-", "-")) {
+				ret_code = rpc_call(sid, name, nfields + 1, params);
+		} else {
+			_rpc_print_usage(2, "Unrecognized option.");
+		}
+
 		if (ret_code == 2) {
 			pinfo("RPC result: %s", (char *) rpc_result);
 			return 0;
 		}
 		pfatal("Something went wrong. No result.");
 		return -1;
+	} else {
+		_rpc_print_usage(0, "Unrecognized option.");
 	}
     _close_keyring();
 }
