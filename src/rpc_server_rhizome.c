6a92ada88d61a5530f3046ba2296162213df96b0
Artur <github@artursterz.de>
2016-09-19 13:47:43 +0200

    Fixed bug where RPCs received via Rhizome could be executed multiple times.

--- src/rpc_server_rhizome.c
+++ src/rpc_server_rhizome.c
@@ -1,5 +1,31 @@
 #include "rpc.h"
 
+char **done_calls;
+int done_calls_size = 0;
+
+// Check if a ID is already in the done list.
+int _rpc_server_rhizome_done (char *id) {
+    int i;
+    for (i = 0; i < done_calls_size; ++i) {
+        if (!strcmp(done_calls[i], id)) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// Add ID to done list.
+void _rpc_server_rhizome_finished (char *id) {
+    if (done_calls_size == 0) {
+        done_calls = malloc(0);
+    }
+    done_calls_size = done_calls_size + 1;
+    done_calls = realloc(done_calls, done_calls_size);
+    size_t id_size = strlen(id);
+    done_calls[done_calls_size - 1] = calloc(id_size + 1, sizeof(char));
+    strcpy(done_calls[done_calls_size - 1], id);
+}
+
 // Send the result via Rhizome
 int _rpc_server_rhizome_send_result (sid_t sid, char *rpc_name, uint8_t *payload) {
     int return_code = 1;
@@ -106,87 +132,97 @@ int _rpc_server_rhizome_process () {
         // ... get the 'rows' entry, ...
         cJSON *rows = cJSON_GetObjectItem(incoming_json, "rows");
         // (if there are no files in the store, abort)
-        if (cJSON_GetArraySize(rows) <= 0) {
+        int	num_files = cJSON_GetArraySize(rows);
+        if (num_files <= 0) {
             return_code = 1;
             goto clean_rhizome_server_listener_all;
         }
-        // ... consider only the recent file, ...
-        cJSON *recent_file = cJSON_GetArrayItem(rows, 0);
-        // ... get the 'service', ...
-        char *service = cJSON_GetArrayItem(recent_file, 2)->valuestring;
-        // ... the sender from the recent file.
-        char *sender = cJSON_GetArrayItem(recent_file, 11)->valuestring;
-        // ... the recipient from the recent file.
-        char *recipient = cJSON_GetArrayItem(recent_file, 12)->valuestring;
 
-        // Check, if this file is an RPC packet and if it is not from but for the client.
-        int service_is_rpc = !strncmp(service, "RPC", strlen("RPC"));
-		int not_my_file = 0;
-		if (recipient) {
-        	not_my_file = recipient != NULL && !strcmp(recipient, alloca_tohex_sid_t(my_subscriber->sid));
-		} else {
-			not_my_file = 1;
-		}
+        int i;
+        for (i = 0; i < num_files; i++) {
+            // ... consider only the recent file, ...
+            cJSON *recent_file = cJSON_GetArrayItem(rows, i);
+            // ... get the 'service', ...
+            char *service = cJSON_GetArrayItem(recent_file, 2)->valuestring;
+            // ... the id, ...
+            char *id = cJSON_GetArrayItem(recent_file, 3)->valuestring;
+            // ... the sender from the recent file.
+            char *sender = cJSON_GetArrayItem(recent_file, 11)->valuestring;
+            // ... the recipient from the recent file.
+            char *recipient = cJSON_GetArrayItem(recent_file, 12)->valuestring;
 
-        // If this is an interesting file: handle it.
-        if (service_is_rpc  && not_my_file) {
-            // Free everyhing, again.
-            _rpc_curl_reinit_memory(&curl_result_memory);
-            curl_slist_free_all(header);
-            header = NULL;
-
-            // Get the bundle ID of the file which should be decrypted.
-            char *bid = cJSON_GetArrayItem(recent_file, 3)->valuestring;
-            char url_decrypt[117];
-            sprintf(url_decrypt, "http://localhost:4110/restful/rhizome/%s/decrypted.bin", bid);
-
-            _rpc_curl_set_basic_opt(url_decrypt, curl_handler, header);
-
-            // Decrypt the file.
-            curl_res = curl_easy_perform(curl_handler);
-            if (curl_res != CURLE_OK) {
-                pfatal("CURL failed (decrypt server process): %s.", curl_easy_strerror(curl_res));
-                return_code = -1;
-                goto clean_rhizome_server_listener_all;
+            // Check, if the call was done already, if this file is an RPC packet and if it is not from but for a client.
+            int not_done = !_rpc_server_rhizome_done(id);
+            int service_is_rpc = !strncmp(service, "RPC", strlen("RPC"));
+            int not_my_file = 0;
+            if (recipient) {
+                not_my_file = recipient != NULL && !strcmp(recipient, alloca_tohex_sid_t(my_subscriber->sid));
+            } else {
+                not_my_file = 1;
             }
 
-            // Copy the payload for manipulations.
-            size_t filesize = cJSON_GetArrayItem(recent_file, 9)->valueint;
-            uint8_t recv_payload[filesize];
-            memcpy(recv_payload, curl_result_memory.memory, filesize);
+            // If this is an interesting file: handle it.
+            if (service_is_rpc && not_my_file && not_done) {
+                // Free everyhing, again.
+                _rpc_curl_reinit_memory(&curl_result_memory);
+                curl_slist_free_all(header);
+                header = NULL;
 
-            if (read_uint8(&recv_payload[0]) == RPC_PKT_CALL) {
-                pinfo("Received RPC via Rhizome.");
-                // Parse the payload to the RPCProcedure struct
-                struct RPCProcedure rp = _rpc_server_parse_call(recv_payload, filesize);
-        		if (str_to_sid_t(&rp.caller_sid, sender) == -1) {
-        			pfatal("Could not convert SID to sid_t. Aborting.");
+                // Get the bundle ID of the file which should be decrypted.
+                char *bid = cJSON_GetArrayItem(recent_file, 3)->valuestring;
+                char url_decrypt[117];
+                sprintf(url_decrypt, "http://localhost:4110/restful/rhizome/%s/decrypted.bin", bid);
+
+                _rpc_curl_set_basic_opt(url_decrypt, curl_handler, header);
+
+                // Decrypt the file.
+                curl_res = curl_easy_perform(curl_handler);
+                if (curl_res != CURLE_OK) {
+                    pfatal("CURL failed (decrypt server process): %s.", curl_easy_strerror(curl_res));
                     return_code = -1;
                     goto clean_rhizome_server_listener_all;
-        		}
-
-                // Check, if we offer this procedure and we should accept the call.
-                if (_rpc_server_offering(&rp) && _rpc_server_accepts(&rp, read_uint32(&recv_payload[1]))) {
-                    pinfo("Offering desired RPC. Sending ACK via Rhizome.");
-
-                    // Compile and send ACK packet.
-                    uint8_t payload[1];
-                    write_uint8(&payload[0], RPC_PKT_CALL_ACK);
-                    _rpc_server_rhizome_send_result(rp.caller_sid, rp.name, payload);
-
-                    // Try to execute the procedure.
-				    uint8_t result_payload[RPC_PKT_SIZE];
-                    memset(result_payload, 0, RPC_PKT_SIZE);
-	                if (_rpc_server_excecute(result_payload, rp)) {
-						pinfo("Sending result via Rhizome.");
-        				_rpc_server_rhizome_send_result(rp.caller_sid, rp.name, result_payload);
-                        pinfo("RPC execution was successful.\n");
-                    }
-                } else {
-                    pwarn("Not offering desired RPC. Ignoring.");
-                    return_code = -1;
                 }
-                _rpc_free_rp(rp);
+
+                // Copy the payload for manipulations.
+                size_t filesize = cJSON_GetArrayItem(recent_file, 9)->valueint;
+                uint8_t recv_payload[filesize];
+                memcpy(recv_payload, curl_result_memory.memory, filesize);
+
+                if (read_uint8(&recv_payload[0]) == RPC_PKT_CALL) {
+                    pinfo("Received RPC via Rhizome.");
+                    // Parse the payload to the RPCProcedure struct
+                    struct RPCProcedure rp = _rpc_server_parse_call(recv_payload, filesize);
+                    if (str_to_sid_t(&rp.caller_sid, sender) == -1) {
+                        pfatal("Could not convert SID to sid_t. Aborting.");
+                        return_code = -1;
+                        goto clean_rhizome_server_listener_all;
+                    }
+
+                    // Check, if we offer this procedure and we should accept the call.
+                    if (_rpc_server_offering(&rp) && _rpc_server_accepts(&rp, read_uint32(&recv_payload[1]))) {
+                        pinfo("Offering desired RPC. Sending ACK via Rhizome.");
+
+                        // Compile and send ACK packet.
+                        uint8_t payload[1];
+                        write_uint8(&payload[0], RPC_PKT_CALL_ACK);
+                        _rpc_server_rhizome_send_result(rp.caller_sid, rp.name, payload);
+
+                        // Try to execute the procedure.
+                        uint8_t result_payload[RPC_PKT_SIZE];
+                        memset(result_payload, 0, RPC_PKT_SIZE);
+                        if (_rpc_server_excecute(result_payload, rp)) {
+                            pinfo("Sending result via Rhizome.");
+                            _rpc_server_rhizome_send_result(rp.caller_sid, rp.name, result_payload);
+                            pinfo("RPC execution was successful.\n");
+                            _rpc_server_rhizome_finished(id);
+
+                        }
+                    } else {
+                        pwarn("Not offering desired RPC. Ignoring.");
+                        return_code = -1;
+                    }
+                    _rpc_free_rp(rp);
+                }
             }
         }
     }
